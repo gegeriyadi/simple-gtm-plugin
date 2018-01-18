948e3cd1196f6c5f6a045cd9982d0385dbff89ba
Teguh Riyadi <gegeriyadi81@gmail.com>
2018-01-18 12:11:19 +0700

    add gtm script

---
+++ public/partials/simple-gtm-plugin-public-display-body.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://gegeriyadi.com/
+ * @since      1.0.0
+ *
+ * @package    Simple_Gtm_Plugin
+ * @subpackage Simple_Gtm_Plugin/public/partials
+ */
+?>
+
+<!-- Generate from Simple GTM Plugin -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $this->containerId ?>"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
---
+++ public/partials/simple-gtm-plugin-public-display-head.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://gegeriyadi.com/
+ * @since      1.0.0
+ *
+ * @package    Simple_Gtm_Plugin
+ * @subpackage Simple_Gtm_Plugin/public/partials
+ */
+?>
+
+<!-- Generate from Simple GTM Plugin -->
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','<?= $this->containerId ?>');</script>
+<!-- End Google Tag Manager -->
